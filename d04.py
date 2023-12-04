fc296b486f5cf1d4b72e22c458de86b33a96d1c9
DrNjitram <martijn.gobes@gmail.com>
2023-12-04 09:34:20 +0100

    better code, courtesy of afdch

--- d04.py
+++ d04.py
@@ -1,18 +1,14 @@
-cards = [line.strip() for line in open("input/d4")]
-cards = [[id, [{int(section) for section in no.split(" ") if section != ''} for no in card.split(": ")[1].split(" | ")]] for id, card in enumerate(cards)]
-original_length = len(cards)
+cards = [[id, [{int(section) for section in no.split(" ") if section != ''} for no in card.split(": ")[1].split(" | ")]] for id, card in enumerate([line.strip() for line in open("input/d4")])]
 
 part1 = 0
-i = 0
-while i < len(cards):
-    id, (winning, numbers) = cards[i]
-    print(i, id)
+copies = [1 for _ in cards]
+for id, (winning, numbers) in cards:
+    print(id)
     winnings = winning & numbers
 
-    if i < original_length: part1 += 2**(len(winnings)-1)
-    cards += cards[id+1:min(original_length, id+len(winnings)+1)]
-
-    i += 1
+    part1 += int(2**(len(winnings)-1))
+    for i in range(len(winnings)):
+        copies[id+i+1] += copies[id]
 
 print(part1)
-print(len(cards))
+print(sum(copies))
